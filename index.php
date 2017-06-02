ee9774c0009aed5b567fa649104172d1fc920efe
Surasak sincharoen <moomdate@gmail.com>
2017-06-02 20:00:53 +0700

    layout desing

--- index.php
+++ index.php
@@ -1,13 +1,65 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+             <link rel="shortcut icon" href="favicon.ico" />
 	<meta charset="UTF-8">
 	<link rel="stylesheet" type="text/css" href="Semantic-UI-CSS-master/semantic.css">
+	<script src="js/jquery.min.js"></script>
+	<script src="Semantic-UI-CSS-master/semantic.js"></script>
 	<title>FireBug</title>
 </head>
 <body>
-	<div class="ui">
-		
+	<div class="ui top fixed menu">
+		<div class="item">
+			<img src="/images/logo.png">
+		</div>
+		<a class="item" href="#dashboard">Dashboard</a>
+		<a class="item" href="#controlpanel">Control penal</a>
+		<a class="item" href="#provider">Provider</a>
 	</div>
+	
+	<p></p>
+	<p></p>
+</br>
+</br>
+<div class="ui container">
+	<div class="ui segments">
+		<div class="ui segment " id="controlpanel">
+			<h3 class="ui header">Control Panel</h3>
+			<div class="ui  segment">
+				<p></p>
+				<div class="ui inverted divider"></div>
+				<p></p>
+				<h4 class="ui horizontal inverted divider">
+					Horizontal
+				</h4>
+			</div>
+
+		</div>
+		<div class="ui segment" id="dashboard">
+			<h3 class="ui header">Dashboard</h3>
+			<div class="ui  segment">
+				<p></p>
+				<div class="ui inverted divider"></div>
+				<p></p>
+				<h4 class="ui horizontal inverted divider">
+					Horizontal
+				</h4>
+			</div>
+		</div>
+		<div class="ui segment" id="provider">
+			<h3 class="ui header">Provider</h3>
+			<div class="ui  segment">
+				<p></p>
+				<div class="ui inverted divider"></div>
+				<p></p>
+				<h4 class="ui horizontal inverted divider">
+					Horizontal
+				</h4>
+			</div>
+		</div>
+		<div class="ui segment" id="mycontent">Content</div>
+	</div>
+</div>
 </body>
 </html>
